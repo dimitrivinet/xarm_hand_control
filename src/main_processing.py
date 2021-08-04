92957a401cf397b6ea54b8e216af6d9df4c43f72
dimitrivinet <dimitri.vinet@edu.devinci.fr>
2021-08-04 20:56:24 +0200

    added comments

--- src/main_processing.py
+++ src/main_processing.py
@@ -3,7 +3,7 @@ import os
 import time
 from collections import deque
 from statistics import mean, mode
-from typing import Any, Tuple
+from typing import Any, Callable, Iterable, Tuple, Union
 
 import cv2
 import dotenv
@@ -32,10 +32,10 @@ MODE = Mode.get(MODE)
 """
 
 # with hard-coded value:
-# MODE = Mode.NO_CLASSIFICATION
+MODE = Mode.NO_CLASSIFICATION
 # MODE = Mode.RANDOM_FOREST
 # MODE = Mode.MLP
-MODE = Mode.ONNX
+# MODE = Mode.ONNX
 
 VIDEO_INDEX = 6
 WINDOW_NAME = "win"
@@ -44,7 +44,7 @@ ROBOT_COMMAND_SCALE = 100
 ROBOT_SPEED = 100.0
 ROBOT_MVACC = 1000.0
 
-MAX_NUM_HANDS=1
+MAX_NUM_HANDS = 1
 
 classification_buffer = deque(maxlen=5)
 fps_buffer = deque(maxlen=50)
@@ -54,11 +54,17 @@ mp_drawing = mp.solutions.drawing_utils
 drawing_styles = mp.solutions.drawing_styles
 
 
-def class_as_str(classes, class_index):
+def class_as_str(classes: dict, class_index: int) -> str:
+    """Get class name from class index
+    """
+
     return classes[class_index]['name']
 
 
 def format_landmarks(landmarks):
+    """Format landmarks to the format used by selected model
+    """
+
     ret = []
 
     for landmark in landmarks:
@@ -67,16 +73,23 @@ def format_landmarks(landmarks):
         if MODE == Mode.RANDOM_FOREST:
             ret.append(np.array([f_landmarks, ]))
 
-        if MODE == Mode.MLP:
+        elif MODE == Mode.MLP:
             ret.append(torch.tensor([f_landmarks, ]))
 
-        if MODE == Mode.ONNX:
+        elif MODE == Mode.ONNX:
             ret.append(np.array([f_landmarks, ], dtype=np.float32))
 
     return ret
 
 
-def get_onnx_session():
+def get_onnx_model() -> Callable[[np.ndarray], list]:
+    """Create the onnx session and return callable used to run inference
+
+    Returns:
+        Callable[[np.ndarray], list]: function to run inference with.
+        Parameter is np.ndarray with shape (1, x, y) and dtype np.float32.
+    """
+
     session = onnxruntime.InferenceSession(ONNX_MODEL_PATH)
     input_name = session.get_inputs()[0].name
     output_name = session.get_outputs()[0].name
@@ -89,25 +102,49 @@ def get_onnx_session():
     return run
 
 
-def load_model(classes=None):
+def load_model(classes: dict = None) -> Any:
+    """Load model according to selected inference mode.
+
+    Args:
+        classes (dict, optional): Classes dict. Defaults to None.
+
+    Returns:
+        Any: Model according to selection
+    """
+
     model = None
 
     if MODE == Mode.RANDOM_FOREST:
         model = joblib.load(RF_MODEL_PATH)
 
-    if MODE == Mode.MLP:
+    elif MODE == Mode.MLP:
         n_classes = len(classes)
         model = HandsClassifier(n_classes)
         model.load_state_dict(torch.load(MLP_MODEL_PATH))
         model.eval()
 
-    if MODE == Mode.ONNX:
-        model = get_onnx_session()
+    elif MODE == Mode.ONNX:
+        model = get_onnx_model()
 
     return model
 
 
-def run_inference(classes: dict, landmarks: Any, model: Any):
+def run_inference(classes: dict, landmarks: Any, model: Any) -> Union[None, list]:
+    """Run inference on array of landmarks with selected model
+
+    Args:
+        classes (dict): Classes dict
+        landmarks (Any): landmarks array
+        model (Any): model selected with MODE
+
+    Returns:
+        list: list of string representing detected classes, buffered to
+        avoid artefacts, None if MODE = Mode.NO_CLASSIFICATION
+    """
+
+    if MODE == Mode.NO_CLASSIFICATION:
+        return None
+
     classified_hands = []
     f_landmarks = format_landmarks(landmarks)
 
@@ -115,26 +152,38 @@ def run_inference(classes: dict, landmarks: Any, model: Any):
 
         if MODE == Mode.RANDOM_FOREST:
             class_index = model.predict(landmark.reshape(1, -1))[0]
-        if MODE == Mode.MLP:
+
+        elif MODE == Mode.MLP:
             class_index = torch.argmax(model(landmark)).item()
-        if MODE == Mode.ONNX:
+
+        elif MODE == Mode.ONNX:
             result = model(landmark)
             class_index = np.argmax(result[0].squeeze(axis=0))
 
         classified_hands.append(class_as_str(classes, class_index))
 
+    # add to buffer and return most common occurence in last n frames
     classification_buffer.appendleft(tuple(classified_hands))
 
     return list(mode(classification_buffer))
 
 
-def run_hands(image, hands):
+def run_hands(image: Any, hands: mp_hands.Hands) -> Tuple[Any, list]:
+    """Run hand landmark recognition on image
+
+    Args:
+        image (Any): Image to run recognition on
+        hands (mp_hands.Hands): Mediapipe Hands instance
+
+    Returns:
+        annotated_image (Any): Image annotated with hand landmarks
+        results.multi_hand_landmarks (list): hand landmarks as list
+    """
+
     # Convert the BGR image to RGB, flip the image around y-axis for correct
     # handedness output and process it with MediaPipe Hands.
-    image.flags.writeable = False
     results = hands.process(
         cv2.flip(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), 1))
-    image.flags.writeable = True
 
     if not results.multi_hand_landmarks:
         return None, None
@@ -148,10 +197,20 @@ def run_hands(image, hands):
             drawing_styles.get_default_hand_connection_style())
 
     return annotated_image, results.multi_hand_landmarks
-    # return cv2.flip(annotated_image, 1), results.multi_hand_landmarks
 
 
-def get_polar_coords(im_shape, landmarks):
+def get_polar_coords(im_shape: Tuple, landmarks: list) -> Tuple[float, float]:
+    """Translate landmarks to polar coordinates with center of palm as middle point
+
+    Args:
+        im_shape (Tuple): Shape of the image used by Mediapipe Hands
+        landmarks (list): Hand landmarks
+
+    Returns:
+        dist (float): Distance from center of palm to center of image
+        angle (float): Angle from vertical axis to center of palm
+    """
+
     im_height = im_shape[0]
     im_width = im_shape[1]
     im_center = (im_width // 2, im_height // 2)
@@ -194,18 +253,30 @@ def get_polar_coords(im_shape, landmarks):
     return dist, angle
 
 
-def get_robot_command(im_shape, dist, angle):
+def get_robot_command(im_shape: Tuple, dist: float, angle: float) -> Command:
+    """Translate distance and angle to an xArm command
+
+    Args:
+        im_shape (Tuple): Shape of the image used by Mediapipe Hands
+        dist (float): Distance from center of palm to center of image
+        angle (float): Angle from vertical axis to center of palm
+
+    Returns:
+        Command: Command NamedTuple containing fields for xArm move command
+    """
+
     command = Command()
 
-    if (dist, angle) == (None, None) or dist < (np.min(im_shape[:2]) / 15):
+    # if center of palm is inside center circle
+    if dist < (np.min(im_shape[:2]) / 15):
         empty_command = Command()
         return empty_command
 
-    im_height, im_width = im_shape[0], im_shape[1]
-    scaled_x = dist * np.cos(np.deg2rad(angle)) / \
-        (im_width / 2) * ROBOT_COMMAND_SCALE
-    scaled_y = dist * np.sin(np.deg2rad(angle)) / \
-        (im_height / 2) * ROBOT_COMMAND_SCALE
+    im_h, im_w = im_shape[0], im_shape[1]
+    scaled_x = \
+        dist * np.cos(np.deg2rad(angle)) / (im_w / 2) * ROBOT_COMMAND_SCALE
+    scaled_y = \
+        dist * np.sin(np.deg2rad(angle)) / (im_h / 2) * ROBOT_COMMAND_SCALE
 
     command = Command(
         x=int(scaled_x),
@@ -217,24 +288,51 @@ def get_robot_command(im_shape, dist, angle):
     return command
 
 
-def run_processing(classes, model, to_show, landmarks):
+def run_processing(classes: dict, model: Any, image: Any, landmarks: list
+                   ) -> Tuple[str, Command]:
+    """Processing loop after Mediapipe Hands ran
+
+    Args:
+        classes (dict): Classes dict
+        model (Any): Model according to selection
+        to_show (Any): Image Mediapipe Hands ran on
+        landmarks (list): Hand landmarks
+
+    Returns:
+        to_show_text (str): Text containing hand classes and distance and angle
+        robot_command (Command): Command NamedTuple for xArm movement
+    """
+
     if landmarks is None:
         return "", None
 
     classified_hands = run_inference(classes, landmarks, model)
 
-    dist, angle = get_polar_coords(to_show.shape, landmarks)
+    dist, angle = get_polar_coords(image.shape, landmarks)
 
-    # classified_hands += f" | {dist:.2f}, {angle:.2f}"
-    to_show_text = " | ".join(
-        classified_hands + [f'{dist:.2f}, {angle:.2f}', ])
 
-    robot_command = get_robot_command(to_show.shape, dist, angle)
+    if classified_hands is None:
+        to_show_text = f'{dist:.2f}, {angle:.2f}'
+    else:
+        classified_hands = ', '.join(classified_hands)
+        to_show_text = " | ".join(
+            [classified_hands, f'{dist:.2f}, {angle:.2f}', ])
+
+    robot_command = get_robot_command(image.shape, dist, angle)
 
     return to_show_text, robot_command
 
 
-def get_classes(dataset_path: os.PathLike = None)-> dict:
+def get_classes(dataset_path: os.PathLike = None) -> dict:
+    """Get classes from dataset JSON
+
+    Args:
+        dataset_path (os.PathLike, optional): Path to dataset JSON. Defaults to None.
+
+    Returns:
+        dict: Classes dict
+    """
+
     if dataset_path is None:
         dataset_path = os.path.join(DATASET_DIR, "dataset.json")
     with open(dataset_path, 'r') as f:
@@ -245,7 +343,17 @@ def get_classes(dataset_path: os.PathLike = None)-> dict:
     return classes
 
 
-def get_fps(prev_frame_time) -> Tuple[float, str]:
+def get_fps(prev_frame_time: float) -> Tuple[float, str]:
+    """Calculate FPS
+
+    Args:
+        prev_frame_time (float): Time when measured at previous loop
+
+    Returns:
+        new_frame_time (float): Time when measured for current loop
+        mean_fps_str (str): FPS as String, buffered to avoid changing too often
+    """
+
     new_frame_time = time.perf_counter()
 
     # calculate fps with mean of last 10 fps
@@ -257,7 +365,14 @@ def get_fps(prev_frame_time) -> Tuple[float, str]:
 
 
 def main():
-    prev_frame_time=0.0
+    """Main loop. Captures video from camera, runs Mediapipe Hands and runs
+    processing before showing image
+
+    Raises:
+        IOError: if OpenCV can't access camera
+    """
+
+    prev_frame_time = 0.0
     win = cv2.namedWindow(WINDOW_NAME, cv2.WINDOW_AUTOSIZE)
 
     classes = get_classes()
